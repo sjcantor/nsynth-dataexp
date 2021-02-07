8acf24f83d758fa4237a3fef184cd710da60ed74
sjcantor <s.cantor@queensu.ca>
2021-02-06 19:40:37 -0500

    Improve dataset download location prompt

--- download_data.sh
+++ download_data.sh
@@ -5,7 +5,7 @@ read -r -p "This will download the NSynth test set (~333MB), which may take a lo
 Are you sure you want to do this? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY])
-	read -p "Enter dataset path [datasets/]: " dataset_path
+	read -p "Enter dataset path [datasets/nsynth-test]: " dataset_path
 	dataset_path=${dataset_path:-datasets}
         mkdir -p $dataset_path
 	curl -o $dataset_path/nsynth_tar $DOWNLOAD_PATH
